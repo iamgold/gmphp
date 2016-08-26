ca2f8ad7b3a79d4c37a1c991339a225f8341f883
Eric Huang <erichuang@talk2yam.com>
2016-08-26 19:41:57 +0800

    Temp for write main class

---
+++ src/GraphicsMagick.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace iamgold\gmphp;
+
+/**
+ * This class is used to create a command for processing
+ * image using Graphics magick.
+ */
+
+class GraphicsMagick
+{
+    /**
+     * Create a command.
+     *
+     * @param string $command if the value is default value[null], will build command
+     *
+     * @return $this
+     */
+    public function createCommand($command=null)
+    {}
+
+    /**
+     * Execute
+     */
+
+}
