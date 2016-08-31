7c6132d71391e576cc88b56248dedab212ad4b0b
Eric Huang <iamgold0105@gmail.com>
2016-08-31 23:21:22 +0800

    Temp for resize image of ConvertCommand

--- src/ConvertCommand.php
+++ src/ConvertCommand.php
@@ -4,6 +4,8 @@ namespace iamgold\gmphp;
 
 /**
  * This class is used to mapulate all options of convert.
+ *
+ * @since 1.1.0
  */
 
 class ConvertCommand extends Command
@@ -16,6 +18,7 @@ class ConvertCommand extends Command
     /**
      * Resize an image
      *
+     * @param string $algorithm values: resize, thumbnail, scale, sample, crop, extent
      * @param int $width
      * @param int $height
      * @param string $fit
@@ -25,16 +28,57 @@ class ConvertCommand extends Command
      *      <: more than original size and must escaping string ex: \<
      *      ^: match, resize the image based on the smallest fitting dimension
      *      %: resize by percent
+     * @param string $offset
+     * @see http://www.graphicsmagick.org/GraphicsMagick.html#details-resize
+     * @since 1.1.1
+     * @return $this
      */
-    public function resize(int $width, int $height, $fit='')
+    public function resizeImage(string $algorithm, int $width, int $height, $fit='', $offset='')
     {
-        if (!empty($fit) && preg_match('/^[\!\>\<\^%]{1}$/', $fit)===false) {
-            throw new InvalidArgumentException("Not support argument of fit ($fit)", 500);
+        if (preg_match('/^(resize|thumbnail|scale|sample|crop|extent)$/', $algorithm)==false)
+            throw new InvalidArgumentException("Not support argument of algorithm ($algorithm). " . __CLASS__ . ':' . __METHOD__, 500);
+
+        if (!$this->validateFit($fit)) {
+            throw new InvalidArgumentException("Not support argument of fit ($fit). " . __CLASS__ . ':' . __METHOD__, 501);
         }
 
-        $value = sprintf('%sx%s%s', $width, $height, $fit);
-        $this->addOption('resize', $value);
+        if (!$this->validateOffset($offset)) {
+            throw new InvalidArgumentException("Not support argument of offset ($offset). " . __CLASS__ . ':' . __METHOD__, 502);
+        }
+
+        $value = sprintf('%sx%s%s%s', $width, $height, $offset, $fit);
+        $this->addOption($algorithm, trim($value));
 
         return $this;
+    }
+
+    /**
+     * Validate fit mode
+     *
+     * @param string $mode
+     * @return bool
+     */
+    private function validateFit(string $fit)
+    {
+        $fit = trim($fit);
+        if (empty($fit))
+            return true;
+
+        return (preg_match('/^[\!\>\<\^%]{1}$/', $fit)!=false);
+    }
+
+    /**
+     * Validate offset
+     *
+     * @param string $offset
+     * @return bool
+     */
+    private function validateOffset(string $offset)
+    {
+        $offset = trim($offset);
+        if (empty($offset))
+            return true;
+
+        return (preg_match('/^[\+-]{1}\d+[\+-]\d+$/', $offset)!=false);
     }
 }
